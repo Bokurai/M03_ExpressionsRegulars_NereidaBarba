2fb83efa27a51aedd54eac28f034e91d03e0d501
Bokurai <mrnere88@gmail.com>
2023-11-06 17:50:13 +0100

    first changes

--- src/MainExpressionsRegulars.java
+++ src/MainExpressionsRegulars.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class MainExpressionsRegulars {
     public static void main(String[] args) {
 
--- src/MainStringMethods.java
+++ src/MainStringMethods.java
@@ -1,5 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class MainStringMethods {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
 
+        File archivo= new File("santako.txt");
+        Scanner lineas = new Scanner(archivo);
+
+        String linea1;
+        String linea2;
+        String linea3;
+        String linea4;
+
+        int contadorpapanoell1 = 0;
+        int contadorpapanoell2 = 0;
+        int contadorrenl2 = 0;
+        int contadorfolletl3 = 0;
+
+
+            linea1 = lineas.nextLine();
+            linea2 = lineas.nextLine();
+            linea3 = lineas.nextLine();
+            linea4 = lineas.nextLine();
+
+            if(linea1.contains("*<]:-DOo")) {
+                contadorpapanoell1++;
+            }
+
+            if(linea2.contains("*<]:-DOo")){
+                contadorpapanoell2++;
+            }
+
+            if(linea2.contains(">:o)")) {
+                contadorrenl2++;
+            }
+
+            if(linea4.contains("<]:-D")) {
+                contadorfolletl3++;
+            }
+
+        System.out.println("Pare Noel (" + contadorpapanoell1 + ")");
+        System.out.println("Pare Noel (" + contadorpapanoell2 + ") Ren(" + contadorrenl2 + ")");
+        System.out.println(" ");
+        System.out.println("Follets (" + contadorfolletl3 + ")");
     }
 }
